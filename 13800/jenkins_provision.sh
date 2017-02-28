fa9118f79f1d3def2b1352671384793651e59adc
Arseny Zinchenko <arseny.zinchenko@akqa.com>
2017-02-28 17:23:39 +0200

    Azure: ARM provisioning и Jenkins в Docker

--- 13800/jenkins_provision.sh
+++ 13800/jenkins_provision.sh
@@ -2,6 +2,6 @@
 
 sudo curl https://get.docker.com/ | bash
 sudo usermod -aG docker serveradmin
-docker run -tid -p 80:8080 -v /jenkins:/jenkins -v /var/run/docker.sock:/var/run/docker.sock -e JENKINS_HOME='/jenkins' --name jenkins jenkins
 mkdir /jenkins
 mount /dev/sdc1 /jenkins/
+docker run -tid -p 80:8080 -v /jenkins:/jenkins -v /var/run/docker.sock:/var/run/docker.sock -e JENKINS_HOME='/jenkins' --name jenkins jenkins
